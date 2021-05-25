b8bf48f044cd4f7d991d78cbd1b69219249568b3
Bobby Cottle <Bobby.Cottle@gmail.com>
2021-05-25 17:09:33 -0400

    Implement special offers (BOGO & 3 for 2)

--- src/main/kotlin/CottlinStore.kt
+++ src/main/kotlin/CottlinStore.kt
@@ -1,19 +1,30 @@
 import java.text.NumberFormat
 
 class CottlinStore {
-    var goods: MutableMap<String, Double> = mutableMapOf()
     val format: NumberFormat = NumberFormat.getCurrencyInstance()
+    var goods: MutableMap<String, Double> = mutableMapOf()
+
+    class SpecialOffer(var itemsBought: Int, var itemsCharged: Int)
+
+    var specials: MutableMap<String, SpecialOffer> = mutableMapOf()
 
     fun start(): Unit {
         println("Welcome to the Cottlin Orders System!")
         goods["apple"] = 0.60
         goods["orange"] = 0.25
+        specials["apple"] = SpecialOffer(2, 1)
+        specials["orange"] = SpecialOffer(3, 2)
     }
 
-    fun takeOrder (): Unit {
+    fun takeOrder(): Unit {
         println("We are selling these items:")
         for ((item, price) in goods) {
-            println("   $item: ${format.format(price)}")
+            print("   $item: ${format.format(price)}   ")
+            var special = specials.getOrDefault(item, null)
+            special?.let {
+                print(" (${special.itemsBought} for ${special.itemsCharged})")
+            }
+            println()
         }
         println("Please enter a space separated list of goods to order from this list:")
         val order = readLine()!!.split(' ').toMutableList()
@@ -23,12 +34,32 @@ class CottlinStore {
     }
 
     fun parseOrder(order: MutableList<String>): Double {
-        val total: Double = order.fold(0.0) { total, item ->
-            var itemPrice = goods.getOrDefault(item, 0.0)
-            if (itemPrice == 0.0) {
-                println("Sorry, we do not have '$item' for sale.")
+        var itemCounts : MutableMap<String, Int> = mutableMapOf()
+
+        var total: Double = 0.0
+
+        // count amount of each item purchased
+        order.forEach {
+            val count = itemCounts.getOrDefault(it, 0) + 1
+            itemCounts[it] = count
+        }
+        // apply discounts where possible, then add any items at full price that were not on special or that weren't enough for a given special
+        itemCounts.forEach {
+            item, count ->
+            val itemPrice = goods.getOrDefault(item, 0.0)
+            val special = specials.getOrDefault(item, null)
+            if (special == null) {
+                total += itemPrice * count
             }
-            total + itemPrice
+            else {
+                // how many full sets purchased at discount
+                val disCount = count / special.itemsBought
+                total += disCount * special.itemsCharged * itemPrice
+
+                // how many left over at full price
+                val fullCount = count % special.itemsBought
+                total += fullCount * itemPrice
+            }
         }
         return total
     }
--- src/test/kotlin/CottlinStoreTest.kt
+++ src/test/kotlin/CottlinStoreTest.kt
@@ -13,9 +13,20 @@ internal class CottlinStoreTest {
 
     @Test
     fun testParseOrder() {
-        var item1List = mutableListOf("banana", "orange", "apple", "apple", "apple")
+        var item1List = mutableListOf("banana", "orange", "orange", "apple")
         var item2List = mutableListOf("apple", "")
-        assertEquals(2.05, store.parseOrder(item1List))
+        assertEquals(0.5 + 0.6, store.parseOrder(item1List))
         assertEquals(0.6, store.parseOrder(item2List))
+    }
+
+    @Test
+    fun testParseOrderWithSpecials() {
+        // 3 oranges, 4 apples
+        var item1List = mutableListOf("orange", "orange", "orange", "apple", "apple", "apple", "apple")
+        assertEquals(0.5 + 1.2, store.parseOrder(item1List))
+
+        // 5 oranges, 3 apples
+        var item2List = mutableListOf("orange", "orange", "orange", "orange", "orange", "apple", "apple", "apple")
+        assertEquals(1.0 + 1.2, store.parseOrder(item2List))
     }
 }
