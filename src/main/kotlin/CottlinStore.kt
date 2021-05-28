cd58bc3ee881da87323b21fd3abd7fbe4a9f96c3
Bobby Cottle <Bobby.Cottle@gmail.com>
2021-05-28 12:39:58 -0400

    use coroutines & channel to implement pub/sub eventing refactor to separate publishing from parsing of order

--- src/main/kotlin/CottlinStore.kt
+++ src/main/kotlin/CottlinStore.kt
@@ -1,14 +1,19 @@
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.SendChannel
 import java.text.NumberFormat
 
-class CottlinStore {
-    val format: NumberFormat = NumberFormat.getCurrencyInstance()
+class CottlinStore(_channel: SendChannel<ChannelMessage>) {
+    private val currencyFormatter: NumberFormat = NumberFormat.getCurrencyInstance()
+    private val scope = CoroutineScope(Job())
+    private val channel = _channel
+
     var goods: MutableMap<String, Double> = mutableMapOf()
 
     class SpecialOffer(var itemsBought: Int, var itemsCharged: Int)
 
     var specials: MutableMap<String, SpecialOffer> = mutableMapOf()
 
-    fun start(): Unit {
+    init {
         println("Welcome to the Cottlin Orders System!")
         goods["apple"] = 0.60
         goods["orange"] = 0.25
@@ -16,27 +21,61 @@ class CottlinStore {
         specials["orange"] = SpecialOffer(3, 2)
     }
 
-    fun takeOrder(): Unit {
+    fun start() {
+        println("Store: starting shop...")
+        while (!channel.isClosedForSend) {
+            var order = takeOrder()
+            if (order.isEmpty()) {
+                channel.close()
+                break
+            }
+            processOrder(order)
+        }
+        println("Store: The Cottlin Orders System is now closed.")
+    }
+
+    private fun takeOrder(): List<String> {
         println("We are selling these items:")
         for ((item, price) in goods) {
-            print("   $item: ${format.format(price)}   ")
-            var special = specials.getOrDefault(item, null)
+            print("   $item: ${currencyFormatter.format(price)}   ")
+            val special = specials.getOrDefault(item, null)
             special?.let {
                 print(" (${special.itemsBought} for ${special.itemsCharged})")
             }
             println()
         }
         println("Please enter a space separated list of goods to order from this list:")
-        val order = readLine()!!.split(' ').toMutableList()
-        val total = parseOrder(order)
 
-        println("Your total comes to ${format.format(total)}.")
+        // ensure no null values
+        val orderLine = readLine() ?: ""
+        if (orderLine.isBlank()) {
+            return emptyList()
+        }
+        return orderLine.split(' ')
     }
 
-    fun parseOrder(order: MutableList<String>): Double {
-        var itemCounts : MutableMap<String, Int> = mutableMapOf()
+    private fun processOrder(order: List<String>) {
+        val (total, badItem) = parseOrder(order)
 
-        var total: Double = 0.0
+        scope.launch {
+            if (total < 0.0) {
+                sendEvent("FAILED", total, "The order contained invalid item [$badItem].")
+            }
+            else {
+                println("Your total comes to ${currencyFormatter.format(total)}.")
+                sendEvent("SUCCESS", total, null)
+            }
+        }
+    }
+
+    /**
+     * Parse the order for valid items and calculate costs, including special offers.
+     *
+     * If any items are invalid, reject order.
+     */
+    fun parseOrder(order: List<String>): Pair<Double, String> {
+        val itemCounts: MutableMap<String, Int> = mutableMapOf()
+        var total = 0.0
 
         // count amount of each item purchased
         order.forEach {
@@ -44,9 +83,12 @@ class CottlinStore {
             itemCounts[it] = count
         }
         // apply discounts where possible, then add any items at full price that were not on special or that weren't enough for a given special
-        itemCounts.forEach {
-            item, count ->
+        for ((item, count) in itemCounts) {
+            // invalid items will void entire order
             val itemPrice = goods.getOrDefault(item, 0.0)
+            if (itemPrice == 0.0) {
+                return Pair(-1.0, item)
+            }
             val special = specials.getOrDefault(item, null)
             if (special == null) {
                 total += itemPrice * count
@@ -61,6 +103,14 @@ class CottlinStore {
                 total += fullCount * itemPrice
             }
         }
-        return total
+        return Pair(total, "OK")
+    }
+
+    private suspend fun sendEvent(type: String, total: Double, msg: String?) {
+        if (channel.isClosedForSend) {
+            return
+        }
+        val cm = ChannelMessage(type, total, msg)
+        channel.send(cm)
     }
 }
