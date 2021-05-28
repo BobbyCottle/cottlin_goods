e3f23855140882f5ef680d72bf8f4bbabae8e356
Bobby Cottle <Bobby.Cottle@gmail.com>
2021-05-28 12:41:59 -0400

    refactor parseOrder to return Pair(total, badItem)

--- src/test/kotlin/CottlinStoreTest.kt
+++ src/test/kotlin/CottlinStoreTest.kt
@@ -1,32 +1,40 @@
+import kotlinx.coroutines.channels.Channel
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
 internal class CottlinStoreTest {
-    var store: CottlinStore = CottlinStore()
+    private val channel = Channel<ChannelMessage>();
+    private var store: CottlinStore = CottlinStore(channel)
 
     @BeforeEach
     fun setUp() {
-        store.start()
     }
+
 
     @Test
     fun testParseOrder() {
-        var item1List = mutableListOf("banana", "orange", "orange", "apple")
-        var item2List = mutableListOf("apple", "")
-        assertEquals(0.5 + 0.6, store.parseOrder(item1List))
-        assertEquals(0.6, store.parseOrder(item2List))
+        // TODO: figure out how to turn off specials for this test
+
+        var invalidItemList1 = mutableListOf("orange", "banana", "apple")
+        assertEquals(Pair(-1.0, "banana"), store.parseOrder(invalidItemList1))
+
+        var invalidItemList2 = mutableListOf("apple", "")
+        assertEquals(Pair(-1.0, ""), store.parseOrder(invalidItemList2))
+
+        var item1List = mutableListOf("orange", "orange", "apple")
+        assertEquals(Pair(0.5 + 0.6, "OK"), store.parseOrder(item1List))
     }
 
     @Test
     fun testParseOrderWithSpecials() {
         // 3 oranges, 4 apples
         var item1List = mutableListOf("orange", "orange", "orange", "apple", "apple", "apple", "apple")
-        assertEquals(0.5 + 1.2, store.parseOrder(item1List))
+        assertEquals(Pair(0.5 + 1.2, "OK"), store.parseOrder(item1List))
 
         // 5 oranges, 3 apples
         var item2List = mutableListOf("orange", "orange", "orange", "orange", "orange", "apple", "apple", "apple")
-        assertEquals(1.0 + 1.2, store.parseOrder(item2List))
+        assertEquals(Pair(1.0 + 1.2, "OK"), store.parseOrder(item2List))
     }
 }
